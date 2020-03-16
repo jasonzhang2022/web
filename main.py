c00a9b93d9a8bb2850a06ac1810e20e2a81f63e4
jason zhang <jasonzhang2002@gmail.com>
2020-03-15 18:02:23 -0700

    workable function

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from flask import  render_template
-from markupsafe import escape
-
-app = flask.Flask(__name__)
-
 
 def index(request):
     return render_template("input.html")
 
 
-@app.route('/calculate', methods=['POST'])
 def calculate(request):
     num = int(request.form["num"])
     result = num*num
