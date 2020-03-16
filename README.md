4d56ccc0f19aef66f03d4093f294fd13b20b488f
jiayi <jiayizhang009@gmail.com>
2020-03-15 20:15:50 -0700

    first code fom nicole

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# web
+***deployment***
+
+```bash
+gcloud functions deploy calculate 
+```
+
+***test***
+
+```bash
+https://us-central1-kubernetes-test-143916.cloudfunctions.net/calculate
+```
--- main.py
+++ main.py
@@ -4,9 +4,17 @@ def index(request):
     return render_template("input.html")
 
 
+
+def addnumber(num):
+    sum = 0
+    for x in range(1, num + 1):
+        sum = sum + x
+    return sum
+
 def calculate(request):
-    num = int(request.form["num"])
-    result = num*num
-    return render_template("result.html", result=result)
-
-
+    num = 0
+    result = 0
+    if request.form and request.form["num"]:
+        num = int(request.form["num"])
+        result = addnumber(num)
+    return render_template("result.html", result=result, num=num)
